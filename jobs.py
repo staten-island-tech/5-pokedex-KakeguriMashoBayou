1ddb2327e98879bf4fc4b0209dfd583e473ab280
KakeguriMashoBayou <boyul@nycstudents.net>
2026-04-01 09:23:30 -0400

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -6,8 +6,12 @@ wards = {
 }
 
 staff={}
+
 for dept, docs in wards.items():
+    print(dept,docs)
     for doc in docs:
         if docs not in staff:
             staff[doc] = dept
+        else:
+            staff[doc].append(dept)
 print(staff)
