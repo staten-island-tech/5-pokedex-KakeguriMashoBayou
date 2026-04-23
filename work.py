aa3231339557890153846f6860280d3ed5b9f28c
KakeguriMashoBayou <boyul@nycstudents.net>
2026-04-23 09:15:15 -0400

    hhtrht

--- work.py
+++ work.py
@@ -7,23 +7,28 @@ moves=open("./moves.json", encoding="utf8")
 data_2= json.load(moves)
 
 
-'''for x in range(0,809):
+for x in range(0,809):
     print(data[x]["name"])
 person = input("Language? Japanese, English, Chinese or French: ").lower()
 
 for x in range(0,809):
-    print(data[x]["name"][person])'''
-data=type
+    print(data[x]["name"][person])
+type = input("what type is your pokemon?: ")
 located = False
 for pokemon in data:
-    if pokemon in pokedex["type"]:
-        print(pokemon["type"]["english"])
+    if type in pokemon["type"]:
+        print(pokemon["name"]["english"])
         located = True
 if not located:
     print("found nothing")
 
-'''name = input("Name Match: ")
+name = input("Name Match: ")
 count = 0 
 for char in data:
     if name in char["name"]["english"]:
-        print(char["name"]["english"])'''
+        print(char["name"]["english"])
+        count += 1
+if count == 0:
+    print("No pokemon goes by that")
+
+abilities = input("what moves are you looking for?: ")
